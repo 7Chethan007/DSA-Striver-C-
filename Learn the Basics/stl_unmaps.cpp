defbd5b0e62492ee70ed9185c52d77fe42ca03fc
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2025-03-31 17:59:46 +0530

    before flush

---
+++ Learn the Basics/stl_unmaps.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // pre-computing
+    unordered_map<int, int> mpp;
+    for(int i = 0; i < n; i++) {
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while(q--) {
+        int num;
+        cin >> num;
+        // fetch
+        cout << mpp[num] << endl;
+    }
+}
